b88e6065f6b207ec9020b73b9b36c01d3ad47adc
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-10-24 12:37:34 +0900

    Fix InputRebinder.cs
    RebindSpaceToEscape 동작이 잘 작동하게 Fix함

--- Assets/Scripts/InputRebinder.cs
+++ Assets/Scripts/InputRebinder.cs
@@ -3,52 +3,35 @@ using UnityEngine.InputSystem;
 
 public class InputRebinder : MonoBehaviour
 {
-
     public InputActionAsset actionAsset;
     private InputAction spaceAction;
 
-    private void Awake()
+    private void OnEnable()
     {
+        Debug.Log("InputRebinder Enable");
         spaceAction = actionAsset.FindAction("Space");
-        spaceAction.Enable();
+        spaceAction?.Enable();
     }
 
     [ContextMenu("Rebind Space To Escape")]
     public void RebindSpaceToEscape()
     {
-        spaceAction.PerformInteractiveRebinding().WithTargetBinding(0).
-            OnMatchWaitForAnother(1f).OnComplete(operation =>
-            {
-                spaceAction.ApplyBindingOverride(0, "<Keyboard>/escape");
-            });
+        int bindingIndex = spaceAction.bindings.IndexOf(x => x.path == "<Keyboard>/space");
+
+        if (bindingIndex != -1)
+        {
+            spaceAction.ChangeBinding(bindingIndex).WithPath("<Keyboard>/escape");
+            Debug.Log("Rebinding Complete: Space to Escape");
+        }
+        else
+        {
+            Debug.Log("Rebinding Failed: Space to Escape");
+        }
     }
 
-
-    //public InputActionAsset actionAsset;
-    //private InputAction spaceAction;
-
-    //void Start()
-    //{
-    //    // [구현사항 1] actionAsset에서 Space 액션을 찾고 활성화합니다.
-
-    //}
-
-    //// [구현사항 2] ContextMenu 어트리뷰트를 활용해서 인스펙터창에서 적용할 수 있도록 함
-
-    //public void RebindSpaceToEscape()
-    //{
-    //    if (spaceAction == null)
-    //        return;
-
-    //    // [구현사항 3] 기존 바인딩을 비활성화하고 새 키로 재바인딩
-
-
-    //    Debug.Log("Done!");
-    //}
-
-    //void OnDestroy()
-    //{
-    //    // 액션을 비활성화합니다.
-    //    spaceAction?.Disable();
-    //}
+    private void OnDisable()
+    {
+        Debug.Log("InputRebinder Disable");
+        spaceAction?.Disable();
+    }
 }
