5c76b90441cb356fcfb7c3347ee974cd9052a7a3
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-10-24 15:22:12 +0900

    Feature SingletonBase & Init AudioManager
    Feature SingletonBase
    Init AudioManager

---
+++ Assets/Scripts/Manager/SingleTonBase.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SingleTonBase<T> : MonoBehaviour where T : Component
+{
+    public T instance;
+
+    public T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject().AddComponent<T>();
+            }
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
