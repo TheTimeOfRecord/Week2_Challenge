625e81021c6c8a7dbe3817aeb55ff20fcb961071
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-10-24 12:36:16 +0900

    Feature space click
    Add PlayerInputController

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    public void OnSpace()
+    {
+        Debug.Log("Jump!");
+    }
+}
